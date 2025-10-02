ff46220762758b117d304f5ca19b99b66ec0a669
davidhung123 <glczi86057@gmail.com>
2025-10-02 17:36:49 +0800

    Create main8.c

---
+++ EX3/main8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1; //output "3.14159元"
+    // int i = 2; //output "6.28319元"
+    // int i = 10; //output "31.41593元"
+    
+    // double k = 0.0;
+    // k = i * 3.1415926535;
+    // printf("%.5f\n", k);
+    
+    printf("%.5f\n", i * 3.1415926535);
+
+    return 0;
+}
